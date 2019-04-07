bec0d9e727d879a2deb4a2712f2c0f32fdbefbae
Fahad Kamraan <fahadkamraan@gmail.com>
2019-04-07 11:25:36 -0700

    Update minervahack.py
    Changed fallback message and callback id. Try to add action if possible rather than text message!

--- minervahack.py
+++ minervahack.py
@@ -5,8 +5,8 @@ webhook_url = 'https://hooks.slack.com/services/THAMM49GB/BHAP4TG9Z/nge1Tct9eA5r
 slack_data = {'text': "Fight, Get the location here", "attachments": [
         {
             "text":"https://www.google.com/maps/place/GitHub/@37.782347,-122.3911146,16z/data=!4m5!3m4!1s0x8085807f619a62df:0x491ce2f73977af35!8m2!3d37.7822671!4d-122.3912479",
-            "fallback": "Plant coin needs a another try!!",
-            "callback_id": "plant_coin",
+            "fallback": "Internet problem, can't get location sorry!!",
+            "callback_id": "ConCVe",
             "color": "#3AA3E3",
             "attachment_type": "default",
         }
